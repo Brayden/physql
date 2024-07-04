3e9c3985741a6e38f3006c58bf07b1773e7b5abd
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-04 13:43:01 -0400

    Puppeteer bandaid

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function generateScreenshot() {
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ executablePath: "chromium-browser" });
     const page = await browser.newPage();
     await page.goto(`./src/index.html`);
     await page.setViewport({ width: 800, height: 480 });

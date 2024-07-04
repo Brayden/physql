ce0929ca208dec9a5774d0ee2a67e91441e4aacf
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-04 16:26:10 -0400

    Try to auto launch python script

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function generateScreenshot() {
-    // const browser = await puppeteer.launch();
-    // const browser = await puppeteer.launch({ executablePath: "chromium-browser" });
-    // const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disabled-setupid-sandbox', '--disable-extensions'] });
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/chromium-browser',
     });
@@ -17,6 +14,17 @@ async function generateScreenshot() {
     await browser.close();
 
     console.log(`Screenshot saved as ${screenshotPath}`);
+
+    // Execute the command `python3 display_image.py`
+    const { exec } = require('child_process');
+    exec(`python3 display_image.py`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
 }
 
 generateScreenshot();

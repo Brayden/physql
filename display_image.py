b633f42927a81f72c5a226500248be22cc7cbf9e
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-06 11:29:55 -0400

    Wait for API to load

--- display_image.py
+++ display_image.py
@@ -5,9 +5,22 @@ from PIL import Image
 
 def main():
     try:
+        # epd = epd7in5_V2.EPD()
+        # epd.init()
+        # epd.Clear()
+
+        # # Open the PNG file and convert it to BMP
+        # png_image = Image.open('page.png')
+        # bmp_image = png_image.convert('1')
+        # bmp_image.save('page.bmp')
+
+        # # Display the BMP image on the e-paper screen
+        # image = Image.open('page.bmp')
+        # epd.display(epd.getbuffer(image))
+        # epd.sleep()
+
         epd = epd7in5_V2.EPD()
         epd.init()
-        epd.Clear()
 
         # Open the PNG file and convert it to BMP
         png_image = Image.open('page.png')
@@ -17,6 +30,11 @@ def main():
         # Display the BMP image on the e-paper screen
         image = Image.open('page.bmp')
         epd.display(epd.getbuffer(image))
+
+        # Use partial update instead of full clear
+        epd.displayPartBaseImage(epd.getbuffer(image))
+        epd.displayPartial(epd.getbuffer(image))
+
         epd.sleep()
 
     except IOError as e:

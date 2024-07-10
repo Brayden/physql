ae7f3829ef081524192b1c9aa9255f91c4e78a85
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-10 08:52:44 -0400

    Updates

--- run_voice_trigger.sh
+++ run_voice_trigger.sh
@@ -5,17 +5,17 @@ python3 -m venv myenv
 source myenv/bin/activate
 
 # Ensure all necessary packages are installed
-pip install SpeechRecognition pyaudio requests waveshare-epd
+pip install SpeechRecognition pyaudio requests waveshare-epd pillow
 
 show_loading_screen() {
-    python3 startup_script.py
-    echo "Startup screen displaying.."
+    python3 display_image.py >> /home/pi/path_to_your_script/logs/display_image.log 2>&1
+    echo "Startup screen displaying.." >> /home/pi/path_to_your_script/logs/display_image.log 2>&1
 }
 
 # Function to run the voice trigger script
 run_voice_trigger() {
     while true; do
-        python3 voice_trigger.py
+        python3 voice_trigger.py >> /home/pi/path_to_your_script/logs/voice_trigger.log 2>&1
         echo "Voice trigger script crashed with exit code $?.  Respawning.." >&2
         sleep 5
     done
@@ -24,7 +24,7 @@ run_voice_trigger() {
 # Function to run the Node.js script
 run_node_script() {
     while true; do
-        node index.js
+        node index.js >> /home/pi/path_to_your_script/logs/node_index.log 2>&1
         echo "Node.js script crashed with exit code $?.  Respawning.." >&2
         sleep 5
     done
